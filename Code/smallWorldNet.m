b0733bec2ebd92fa146c3dd8975008d7bfcb6c25
DwyaneRun <39941430+DwyaneRun@users.noreply.github.com>
2018-06-29 19:30:03 +0800

    操你妈

--- Code/smallWorldNet.m
+++ Code/smallWorldNet.m
@@ -1,6 +1,6 @@
 function result = smallWorldNet(casedata,N,m,p,Rm,Xm)
 matrix = zeros(N,N);
-r = rand(N,N)*Rm;
+R = rand(N,N)*Rm;
 X = rand(N,N)*Xm;
 for i=m+1:N- m
     matrix(i,i- m:i+m)=1;
@@ -15,15 +15,12 @@ for i=1:m
     matrix(i,N- m+i:N)=1;
     matrix(N- m+i:N,i)=1;
 end
-k=(rand(N)<p);
-kk=tril(k,-1);
-kkk=triu(k',0);
-kkkk=kkk+kk;
+kk=tril((rand(N)<p),-1)+triu((rand(N)<p)',0);
 for i=1:N
-    kkkk(i,i)=0;
+    kk(i,i)=0;
 end
 % Random add edge
-matrix = logical(matrix + kkkk);
+matrix = logical(matrix + kk);
 matrix = matrix -diag(diag(matrix));
 count=0;
 for i=1:N
@@ -31,15 +28,15 @@ for i=1:N
         count=count+matrix(i,j);
     end
 end
-count=count/2;
-last=zeros(count,4);
+count=count/2;      
+last=zeros(count,4); %输出的结果
 ah=1;
 for i=1:N
     for j=i+1:N
         if matrix(i,j)==1
             last(ah,1)=i;
             last(ah,2)=j;
-            last(ah,3)=r(i,j);
+            last(ah,3)=R(i,j);
             last(ah,4)=X(i,j);
             ah=ah+1;
         end
